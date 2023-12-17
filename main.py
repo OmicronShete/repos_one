9e08fe5d93ee9fc721d376b16e064100b68e1c0d
OmicronShete <omjshete0116@gmail.com>
2023-12-17 18:54:53 +0530

    git commit

--- main.py
+++ main.py
@@ -11,11 +11,6 @@ import shutil
 model = YOLO("yolov8s.pt","v8")
 
 
-# go back
-def goback():
-    pass
-
-
 # browse image function & take file path
 def browse_image():
     global image_path
@@ -76,6 +71,217 @@ def get_name():
 
     else:
         print("No Input's Detected !!")
+
+
+
+# skinny type 
+def skinny():
+
+    # high calorie
+    def shigh_calorie():
+        
+        high_calorie_diet1 = ["Whole grains", "Lean proteins(chicken, fish, tofu)", "Healthy fats(avocado, nuts, olive oil)", "Dairy products", "Fruits and vegetables", "Protein shakes", "Snack on nuts and seeds"]
+
+        label2 = tk.Label(root, text="High Calorie Diet\nFor Skinny Body Type :\n", fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label2.place(x = 900, y = 100)
+
+        
+        high_calorie_diet_str = "\n".join(high_calorie_diet1)
+
+        label_high = tk.Label(root, text=high_calorie_diet_str, wraplength=250, justify='left', fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label_high.place(x = 900, y = 150)
+
+
+    # protein rich
+    def sprotein_rich():
+        protein_rich_diet1 = ["Lean meats (chicken, turkey, lean beef)", "Fish and seafood", "Eggs", "Dairy products (Greek yogurt, cheese)", "Legumes (beans, lentils)", "Quinoa", "Protein bars"]
+
+
+        label3 = tk.Label(root, text="Protein Rich Diet\nFor Skinny Body Type :\n", fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label3.place(x = 900, y = 100)
+
+        
+        high_calorie_diet_str = "\n".join(protein_rich_diet1)
+
+        label_high = tk.Label(root, text=high_calorie_diet_str, wraplength=250, justify='left', fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label_high.place(x = 900, y = 150)
+
+
+
+    # high calorie button
+    highcal_button = tk.Button(root, text="High Calorie Diet", fg="yellow", bg="red",
+                               command=shigh_calorie,
+                                font=("JetBrains Mono", 17), justify="center")
+    highcal_button.place(x = 450, y = 180, height=55, width=300)
+
+
+    # protein rich button
+    prorich_button = tk.Button(root, text="Protein Rich Diet", fg="yellow", bg="red",
+                               command=sprotein_rich,
+                                font=("JetBrains Mono", 17), justify="center")
+    prorich_button.place(x = 450, y = 285, height=55, width=300)
+
+
+
+# fit type
+def fit():
+
+    # high calorie
+    def fhigh_calorie():
+        
+        high_calorie_diet2 = ["Lean meats (chicken, turkey, lean beef)", "Fish and seafood", "Eggs", "Dairy products (Greek yogurt, cheese)", "Whole grains (brown rice, quinoa)", "Healthy fats (avocado, nuts, olive oil)", "Fruits and vegetables", "Protein shakes"]
+
+
+        label2 = tk.Label(root, text="High Calorie Diet\nFor Fit Body Type :\n", fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label2.place(x = 900, y = 100)
+
+        
+        high_calorie_diet_str = "\n".join(high_calorie_diet2)
+
+        label_high = tk.Label(root, text=high_calorie_diet_str, wraplength=250, justify='left', fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label_high.place(x = 900, y = 150)
+
+
+    # protein rich
+    def fprotein_rich():
+        protein_rich_diet2 = ["Lean meats (chicken, turkey, lean beef)", "Fish and seafood", "Eggs", "Dairy products (Greek yogurt, cheese)", "Legumes (beans, lentils)", "Quinoa", "Nuts and seeds", "Protein bars"]
+
+        label3 = tk.Label(root, text="Protein Rich Diet\nFor Fit Body Type :\n", fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label3.place(x = 900, y = 100)
+
+        
+        fprotein_rich_str = "\n".join(protein_rich_diet2)
+
+        label_high = tk.Label(root, text=fprotein_rich_str, wraplength=250, justify='left', fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label_high.place(x = 900, y = 150)
+
+
+
+    # high calorie button
+    highcal_button = tk.Button(root, text="High Calorie Diet", fg="yellow", bg="red",
+                               command=fhigh_calorie,
+                                font=("JetBrains Mono", 17), justify="center")
+    highcal_button.place(x = 450, y = 180, height=55, width=300)
+
+
+    # protein rich button
+    prorich_button = tk.Button(root, text="Protein Rich Diet", fg="yellow", bg="red",
+                               command=fprotein_rich,
+                                font=("JetBrains Mono", 17), justify="center")
+    prorich_button.place(x = 450, y = 285, height=55, width=300)
+
+
+
+# muscular type
+def muscular():
+    # high calorie
+    def mhigh_calorie():
+        
+        high_calorie_diet3 = ["Lean meats (chicken, turkey, lean beef)", "Fish and seafood", "Eggs", "Dairy products (Greek yogurt, cheese)", "Whole grains (brown rice, quinoa)", "Healthy fats (avocado, nuts, olive oil)", "Fruits and vegetables", "Protein shakes"]
+
+        label2 = tk.Label(root, text="High Calorie and\nProtein Rich Diet for\nMuscular Body Type :\n", fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label2.place(x = 900, y = 100)
+
+        
+        high_calorie_diet_str = "\n".join(high_calorie_diet3)
+
+        label_high = tk.Label(root, text=high_calorie_diet_str, wraplength=250, justify='left', fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label_high.place(x = 900, y = 150)
+
+
+    # protein rich
+    def muscle_building():
+        muscle_building_diet = ["High-quality protein sources (chicken, fish, eggs)", "Complex carbohydrates (sweet potatoes, whole grains)", "Healthy fats (olive oil, nuts)", "Vegetables", "Fruits", "Dairy products (Greek yogurt, milk)", "Protein bars"]
+
+
+        label3 = tk.Label(root, text="Muscle Building Diet\nFor Muscular Body Type :\n", fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label3.place(x = 900, y = 100)
+
+        
+        muscle_building_str = "\n".join(muscle_building_diet)
+
+        label_high = tk.Label(root, text=muscle_building_str, wraplength=250, justify='left', fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label_high.place(x = 900, y = 150)
+
+
+
+    # high calorie button
+    highcal_button = tk.Button(root, text="High Calorie & \nProtein Rich Diet", fg="yellow", bg="red",
+                               command=mhigh_calorie,
+                                font=("JetBrains Mono", 17), justify="center")
+    highcal_button.place(x = 450, y = 180, height=65, width=300)
+
+
+    # protein rich button
+    prorich_button = tk.Button(root, text="Mass Building Diet", fg="yellow", bg="red",
+                               command=muscle_building,
+                                font=("JetBrains Mono", 17), justify="center")
+    prorich_button.place(x = 450, y = 285, height=55, width=300)
+
+
+
+# bulk type
+def bulk():
+    # high calorie
+    def bhigh_calorie():
+        
+        high_calorie_diet4 = ["Lean meats (chicken, turkey, lean beef)", "Fish and seafood", "Eggs", "Dairy products (Greek yogurt, cheese)", "Whole grains (brown rice, quinoa)", "Healthy fats (avocado, nuts, olive oil)", "Fruits and vegetables", "Protein shakes"]
+
+        label2 = tk.Label(root, text="High Calorie Diet\nFor Bulk Body Type :\n", fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label2.place(x = 900, y = 100)
+
+        
+        high_calorie_diet_str = "\n".join(high_calorie_diet4)
+
+        label_high = tk.Label(root, text=high_calorie_diet_str, wraplength=250, justify='left', fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label_high.place(x = 900, y = 150)
+
+
+    # protein rich
+    def bmass_building():
+        serious_mass_building_diet = ["High-quality protein sources (chicken, fish, eggs)", "Complex carbohydrates (sweet potatoes, whole grains)", "Healthy fats (olive oil, nuts)", "Vegetables", "Fruits", "Dairy products (Greek yogurt, milk)", "Protein bars", "Calorie-dense snacks"]
+
+
+        label3 = tk.Label(root, text="Serious Mass Building Diet\nFor Bulk Body Type :\n", fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label3.place(x = 900, y = 100)
+
+        
+        muscle_building_str = "\n".join(serious_mass_building_diet)
+
+        label_high = tk.Label(root, text=muscle_building_str, wraplength=250, justify='left', fg="black", bg="blue", 
+                        font=("JetBrains Mono", 15, "bold"))
+        label_high.place(x = 900, y = 150)
+
+
+
+    # high calorie button
+    highcal_button = tk.Button(root, text="High Calorie Diet", fg="yellow", bg="red",
+                               command=bhigh_calorie,
+                                font=("JetBrains Mono", 17), justify="center")
+    highcal_button.place(x = 450, y = 180, height=55, width=300)
+
+
+    # protein rich button
+    prorich_button = tk.Button(root, text="Protein Rich Diet", fg="yellow", bg="red",
+                               command=bmass_building,
+                                font=("JetBrains Mono", 17), justify="center")
+    prorich_button.place(x = 450, y = 285, height=55, width=300)
+
 
 
 
@@ -172,9 +378,6 @@ def get_fruit_info_name(fruit_name1):
         label_name_2.place(x = 480, y = 150)
         # item_found()
 
-        back_button = tk.Button(root, text="GO BACK", fg="yellow", bg="red",
-                                font=("JetBrains Mono", 17), justify="center")
-        back_button.place(x = 1000, y = 570)
 
     else:
         # print(f"{fruit_name} Not Found !")
@@ -197,31 +400,30 @@ def suggest():
 
     # skinny 
     skinny_button = tk.Button(root, text="SKINNYY", fg="black", bg="skyblue", justify="center",
-                            #   command=skinny,
+                              command=skinny,
                                 font=("JetBrains Mono", 17))
     skinny_button.place(x = 70, y = 180, height=50, width=250)
 
 
     # Fit
     fit_button = tk.Button(root, text="FIT", fg="black", bg="skyblue",
-                        #    command=fit,
+                           command=fit,
                                 font=("JetBrains Mono", 17), justify="center")
     fit_button.place(x = 70, y = 260, height=50, width=250)
 
 
     # Muscular
     muscular_button = tk.Button(root, text="MUSCULAR", fg="black", bg="skyblue",
-                                # command=muscular,
+                                command=muscular,
                                 font=("JetBrains Mono", 17), justify="center")
     muscular_button.place(x = 70, y = 340, height=50, width=250)
 
 
     # Bulk
     bulk_button = tk.Button(root, text="BULK", fg="black", bg="skyblue",
-                            # command=bulk,
+                            command=bulk,
                                 font=("JetBrains Mono", 17), justify="center")
     bulk_button.place(x = 70, y = 420, height=50, width=250)
-
 
 
 
